0e21651f1d119f06e4b14bd2277731ded071d6fe
wugaofei <wugaofei@58.com>
2020-09-08 19:54:34 +0800

    fix: cange msg

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to 58FE"/>
+    <HelloWorld msg="Welcome to Your Vue.js"/>
   </div>
 </template>
 
