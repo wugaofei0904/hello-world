011cffe136cea61ab9d4b150f65e8d8b788df4c2
wugaofei0904 <34853276+wugaofei0904@users.noreply.github.com>
2020-10-20 19:41:09 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js"/>
+    <HelloWorld msg="Welcome to Your Vue.js 666"/>
   </div>
 </template>
 
